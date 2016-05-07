c0532b207b7373854e84cd0cc993ad559b02fb18
libeclipse <libeclipse@gmail.com>
2016-05-07 17:06:36 +0000

    Added to pypi.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='importfromtwitter',
+      version='1.0.0',
+      description='Host and import your python functions from Twitter!',
+      long_description='Disclaimer: This is not a serious project, just a bit of fun.',
+      url='https://github.com/libeclipse/import-from-twitter',
+      author='libeclipse',
+      author_email='libeclipse@gmail.com',
+      license='MIT',
+      packages=['importfromtwitter'],
+      install_requires=["beautifulsoup4", "requests"],
+      keywords = ['importfromtwitter', 'import', 'from', 'twitter', 'host'],
+      zip_safe=False)
